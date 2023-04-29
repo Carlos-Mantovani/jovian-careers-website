8ca1566db91265e5f18a6ff5f2b5493ec7836607
Carlos-Mantovani <sherifinho.augusto@gmail.com>
2023-04-29 17:34:42 -0300

    Adding dynamic data for jobs in the page and creating API route

--- app.py
+++ app.py
@@ -1,10 +1,41 @@
-from flask import Flask, request, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+JOBS = [
+	{
+		'id': 1,
+		'title': 'Data Analyst',
+		'location': 'Texas, USA',
+		'salary': '$ 4,000.00'	
+	},
+	{
+		'id': 2,
+		'title': 'Data Scientist',
+		'location': 'San Francisco, USA',
+		'salary': '$ 7,000.00'	
+	},
+	{
+		'id': 3,
+		'title': 'Frontend Engineer',
+		'location': 'Remote',
+		'salary': '$ 5,000.00'	
+	},
+	{
+		'id': 4,
+		'title': 'Backend Engineer',
+		'location': 'Remote',
+		'salary': '$ 10,000.00'	
+	}
+]
+
 @app.route('/')
 def index():
-	return 'Hello'
+	return render_template('index.html', jobs=JOBS)
+
+@app.route('/jobs')
+def list_jobs():
+	return jsonify(JOBS)
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', debug=True)
